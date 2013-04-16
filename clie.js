f73d8e64d46359eaf557f26eb8c37d42da12def7
nakedible <naked@iki.fi>
2013-04-16 12:19:42 +0300

    Add beginnings of command-line parsing.

--- clie.js
+++ clie.js
@@ -1,4 +1,21 @@
+#!/usr/bin/node
 "use strict";
+
+require('streamline').register({
+//    fibers: true,
+    cache: true,
+    verbose: false
+});
+
+var program = require('commander');
+
+program
+    .version('0.0.1')
+    .option('-d, --debug', 'enable debugging')
+    .option('-o, --one-shot', 'run log synchronization in one shot mode')
+    .option('-f, --config <path>', 'path to configuration file')
+    .option('-b, --base <path>', 'treat base as filesystem root')
+    .parse(process.argv);
 
 var client = require('./client');
 
