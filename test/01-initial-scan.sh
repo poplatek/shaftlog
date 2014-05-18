3c9df8362f2bc6e6417f19304ca4a51c83542bb4
nakedible <naked@iki.fi>
2014-05-18 23:27:18 +0300

    Add first test script and modify test runner wrapper.

---
+++ test/01-initial-scan.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+set -e
+
+cleanup() {
+    rm -rf scandir datadir servdir client.log server.log client.yaml server.yaml a.txt b.txt
+}
+trap 'cleanup; exit' INT QUIT TERM
+
+cleanup
+
+cat <<EOF >./client.yaml
+datadir: ./datadir
+logfile: ./client.log
+scan_paths:
+- name: test
+  pattern: ./scandir/test*
+destinations:
+- url: http://localhost:10655/{hostname}-a/
+- url: http://localhost:10655/{hostname}-b/
+EOF
+
+cat <<EOF >./server.yaml
+datadir: ./servdir
+logfile: ./server.log
+EOF
+
+mkdir scandir
+mkdir datadir
+echo "foo" > scandir/test
+sleep 2;
+echo "bar" > scandir/test.1
+sleep 2;
+
+mkdir servdir
+../shaftlog-server.js -dsf ./server.yaml & server_pid=$!
+../shaftlog-client.js -dsf ./client.yaml & client_pid=$!
+
+sleep 2
+
+kill $client_pid
+wait $client_pid
+kill $server_pid
+wait $server_pid
+
+ls -i ./scandir | cut -d' ' -f1 | sort -n > a.txt
+ls -i ./datadir | cut -d' ' -f1 | sort -n > b.txt
+
+diff a.txt b.txt
+
+diff -r ./datadir ./servdir/`hostname`-a/
+diff -r ./datadir ./servdir/`hostname`-b/
+
+cleanup
--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-echo "dummy test"
+set -e
+
+installroot="$(dirname "$(readlink -f "$0")")"
+
+cd "$installroot"
+sh ./01-initial-scan.sh
+
 exit 0
